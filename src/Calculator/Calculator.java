14a5245f35f7249bff879a359c923bdef14b6479
drewpyun <48503175+drewpyun@users.noreply.github.com>
2019-03-20 23:44:30 -0400

    Initial Commit Created a simple Calc GUI but nothing has been coded yet Created a simple main method

---
+++ src/Calculator/Calculator.java
@@ -0,0 +1,52 @@
+package Calculator;
+
+import javax.swing.*;
+
+public class Calculator {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JButton HYPButton;
+    private JButton a2ndButton;
+    private JButton DRGButton;
+    private JButton LOGButton;
+    private JButton offButton;
+    private JButton onCButton;
+    private JButton SINButton;
+    private JButton COSButton;
+    private JButton yXButton;
+    private JButton PIButton;
+    private JButton a1XButton;
+    private JButton x2Button;
+    private JButton LNButton;
+    private JButton TANButton;
+    private JButton sqrtXButton;
+    private JButton eButton;
+    private JButton eeButton;
+    private JButton button18;
+    private JButton button19;
+    private JButton STOButton;
+    private JButton a7Button;
+    private JButton RCLButton;
+    private JButton aBCButton;
+    private JButton button24;
+    private JButton a4Button;
+    private JButton a8Button;
+    private JButton a9Button;
+    private JButton a5Button;
+    private JButton a6Button;
+    private JButton a1Button;
+    private JButton a2Button;
+    private JButton a3Button;
+    private JButton button33;
+    private JButton button34;
+    private JButton a0Button;
+    private JButton button14;
+    private JButton button36;
+    private JButton button37;
+    private JButton button1;
+    private JButton button2;
+
+    public Calculator() {
+        System.out.println("x");
+    }
+}
---
+++ src/Calculator/Main.java
@@ -0,0 +1,18 @@
+package Calculator;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        System.out.println("hello!");
+        Calculator g = new Calculator();
+        //todo see how to put images on the Jpanel as a background
+        //todo button free move
+        //todo call calculator method
+        //todo execute and show the actual GUI
+        //todo start adding functions to Calculator
+    }
+
+}
